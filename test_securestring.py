2d022b6a29ff25ef43109c60097e2c1fa546ffd3
aznashwan <aznashwan@yahoo.com>
2014-08-10 19:31:38 +0300

    Addic basic outline of the tests

---
+++ test_securestring.py
@@ -0,0 +1,46 @@
+from subprocess import check_output
+from securestring import encrypt, decrypt
+
+testinputs = [
+	"Simple",
+	"A longer string",
+	"A!string%with(a4239lot#of$&*special@characters{[]})",
+	"Quite a very much longer string meant to push the envelope",
+	"fsdafsgdfgdfgdfgdfgsdfgdgdfgdmmghnh kv dfv dj fkvjjenrwenvfvvslfvnsljfvnlsfvlnsfjlvnssdwoewivdsvmxxvsdvsdv",
+]
+
+# tests whether encryption and decryption are symmetrical operations
+def test_encryption_decryption_symmetry():
+	for input in testinputs:
+		try:
+			assert decrypt(encrypt(input)) == input
+		except:
+			assert False
+
+
+def runpscommand(command):
+	try:
+		return check_output(["powershell.exe", "-NoProfile", "-NonInteractive",
+		"try{$input|iex; exit $LastExitCode}catch{Write-Error -Message $Error[0]; exit 1}",
+		command]).encode("utf-8")
+	except:
+		raise Exception("System error has occured.")
+
+# tests whether decrypt() can handle input directly from ConvertFrom-SecureString
+def test_decrypt_from_CFSS():
+	for input in testinputs:
+		try:
+			psenc = runpscommand("ConvertTo-SecureString \"%s\" -AsPlainText -Force | ConvertFrom-SecureString" % input)
+			assert decrypt(psenc) == input
+		except:
+			assert False
+
+# tests whether the output of encrypt() is compatible with ConvertTo-SecureString
+def test_convert_encrypted_to_securestring():
+	for input in testinputs:
+		try:
+			enc = encrypt(input)
+			psres = runpscommand("\"%s\" | ConvertTo-SecureString", enc)
+			assert psres == "System.Security.SecureString"
+		except:
+			assert False
